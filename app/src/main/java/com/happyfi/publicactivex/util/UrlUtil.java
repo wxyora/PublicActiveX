7bbef10d8af606f9207105ea8db7b419fb946ccb
wxyora@sina.com <wxyora@sina.com>
2016-03-28 23:54:29 +0800

    success

--- app/src/main/java/com/happyfi/publicactivex/util/UrlUtil.java
+++ app/src/main/java/com/happyfi/publicactivex/util/UrlUtil.java
@@ -8,6 +8,7 @@ public class UrlUtil {
     public static String TaoBaoLoginUrl = "https://login.m.taobao.com/login.htm";
     public static String TaoBaoHostUrl = "http://h5.m.taobao.com/mlapp/mytaobao.html#mlapp-mytaobao";
 
+
     public static String TaoBaoAddessUrl = "http://h5.m.taobao.com/mtb/address.html";
 //http://h5.m.taobao.com/mtb/address.html?spm=a2141.7756461.0.0
 
