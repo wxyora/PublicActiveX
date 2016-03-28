16a142f2f19e4a4c6494025cfc987963474f2694
Acer-002 <wxyora@sina.com>
2016-03-28 10:20:51 +0800

    11

--- app/src/main/java/com/happyfi/publicactivex/activity/VerifySuccessActivity.java
+++ app/src/main/java/com/happyfi/publicactivex/activity/VerifySuccessActivity.java
@@ -1,15 +1,29 @@
 package com.happyfi.publicactivex.activity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.happyfi.publicactivex.R;
 
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_verify_success)
 public class VerifySuccessActivity extends BaseActivity {
+
+    @ViewInject(R.id.verify_result_id)
+    private TextView verify_result_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_success);
+        String root = getIntent().getStringExtra("root");
+        if(root.equals("taobao")){
+            verify_result_id.setText("淘宝认证成功");
+        }else if(root.equals("jingdong")){
+            verify_result_id.setText("京东认证成功");
+        }
     }
 
     @Override
