cac6c521f58a64548b3334379c7ed25899f0fb26
Acer-002 <wxyora@sina.com>
2016-03-30 11:07:47 +0800

    11

--- app/src/main/java/com/happyfi/publicactivex/activity/TaoBaoActivity.java
+++ app/src/main/java/com/happyfi/publicactivex/activity/TaoBaoActivity.java
@@ -1,23 +1,13 @@
 package com.happyfi.publicactivex.activity;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.happyfi.publicactivex.R;
-import com.happyfi.publicactivex.util.UrlUtil;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 
@@ -61,7 +51,7 @@ public class TaoBaoActivity extends BaseActivity {
                     startActivity(i);
                     finish();
                 }*/
-                if(!url.contains("taobao://h5.m.taobao.com/awp")){
+                if (!url.contains("taobao://h5.m.taobao.com/awp")) {
                     view.loadUrl(url);
                 }
 
@@ -69,29 +59,28 @@ public class TaoBaoActivity extends BaseActivity {
             }
 
 
-
             @Override
             public void onLoadResource(WebView view, String url) {
                 super.onLoadResource(view, url);
 
                 //获取等级
-                if(url.contains("png_q90")){
+                if (url.contains("png_q90")) {
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    view.loadUrl("javascript:window.local_obj.showSource(" + "document.getElementsByClassName('user-nick')[0].innerHTML);");
+                    view.loadUrl("javascript:window.local_obj.showSource(document.getElementsByClassName('user-nick')[0].innerHTML,'userLevel');");
                     view.loadUrl("https://h5.m.taobao.com/mtb/address.html");
                 }
                 //获取收获地址
-                if(url.contains("https://h5.m.taobao.com/favicon.ico")){
-                   try {
+                if (url.contains("https://h5.m.taobao.com/favicon.ico")) {
+                    try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    view.loadUrl("javascript:window.local_obj.showSource(" + "document.getElementsByTagName('html')[0].innerHTML);");
+                    view.loadUrl("javascript:window.local_obj.showSource(document.getElementsByTagName('html')[0].innerHTML,'addressList');");
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
@@ -101,27 +90,26 @@ public class TaoBaoActivity extends BaseActivity {
                 }
 
                 //获取订单详情
-                if(url.contains("https://gw.alicdn.com/tps/i2")){
+                if (url.contains("https://gw.alicdn.com/tps/i2")) {
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    view.loadUrl("javascript:window.local_obj.showSource(" + "document.getElementsByClassName('order-box order-message')[0].innerHTML);");
-
+                    view.loadUrl("javascript:window.local_obj.showSource(document.getElementsByClassName('order-box order-message')[0].innerHTML,'orderDetail');");
 
                 }
 
 
                 //获取订单列表
-                if(url.contains("https://h5.m.taobao.com/mlapp/favicon.png")&&runOneTime==false){
+                if (url.contains("https://h5.m.taobao.com/mlapp/favicon.png") && runOneTime == false) {
 
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    view.loadUrl("javascript:window.local_obj.showSource(" + "document.getElementsByClassName('order-list')[0].innerHTML);");
+                    view.loadUrl("javascript:window.local_obj.showSource(document.getElementsByClassName('scroll-content')[0].innerHTML,'orderList');");
 
                     view.loadUrl("https://h5.m.taobao.com/mlapp/odetail.html?bizOrderId=1749316022573844");
                     runOneTime = true;
@@ -169,26 +157,51 @@ public class TaoBaoActivity extends BaseActivity {
 
     final class InJavaScriptLocalObj {
         @JavascriptInterface
-        public void showSource(String html) {
-            System.out.println("记录数据" + html);
+        public void showSource(String html, String dataType) {
+            getParaValue(html, dataType);
         }
     }
 
-    public List<HashMap<String, String>> getParaValue(String source) {
+    public List<HashMap<String, String>> getParaValue(String html, String dataType) {
         List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-        String optionRegExp1 = "<li data-username=\"(.*?)\" data-address=\"(.*?)\".*?<label name=\"phone-num\" style=\"float: right\">(.*?)</label>";
-        Matcher matcher1 = Pattern.compile(optionRegExp1).matcher(source);
-
-        while (matcher1.find()) {
-            HashMap<String, String> map = new HashMap<String, String>();
-            String name = matcher1.group(1);
-            String address = matcher1.group(2);
-            String phone = matcher1.group(3);
-            map.put("name", name);
-            map.put("address", address);
-            map.put("phone", phone);
-            list.add(map);
+        if (dataType.equals("userLevel")) {
+            String optionRegExp1 = "<p class=\"(.*?)\" id=\"J_myNick\">(.*?)</p> <p class=\"(.*?)\"></p>";
+            Matcher matcher1 = Pattern.compile(optionRegExp1).matcher(html);
+            while (matcher1.find()) {
+                HashMap<String, String> map = new HashMap<String, String>();
+                String level = matcher1.group(3);
+                map.put("level", level.substring(6,level.length()));
+                list.add(map);
+            }
+        } else if (dataType.equals("addressList")) {
+            String optionRegExp1 = "<li data-username=\"(.*?)\" data-address=\"(.*?)\".*?<label name=\"phone-num\" style=\"float: right\">(.*?)</label>";
+            Matcher matcher1 = Pattern.compile(optionRegExp1).matcher(html);
+            while (matcher1.find()) {
+                HashMap<String, String> map = new HashMap<String, String>();
+                String name = matcher1.group(1);
+                String address = matcher1.group(2);
+                String phone = matcher1.group(3);
+                map.put("name", name);
+                map.put("address", address);
+                map.put("phone", phone);
+                list.add(map);
+            }
+        } else if (dataType.equals("orderList")) {
+            String optionRegExp1 = "module (\\d*)_1 item";
+            Matcher matcher1 = Pattern.compile(optionRegExp1).matcher(html);
+            while (matcher1.find()) {
+                HashMap<String, String> map = new HashMap<String, String>();
+                String orderNo = matcher1.group(1);
+                map.put(orderNo, orderNo);
+                list.add(map);
+            }
+        } else if (dataType.equals("orderDetail")) {
+            System.out.println("记录数据" + dataType + html);
         }
-        return list;
+        System.out.println("记录数据" + dataType + list);
+        return null;
     }
+
+
+
 }
