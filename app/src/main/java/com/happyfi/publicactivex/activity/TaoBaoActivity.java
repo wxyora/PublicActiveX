3183ccded66eee9efbdad329ef16889713828cce
wxyora@sina.com <wxyora@sina.com>
2016-04-03 12:43:18 +0800

    success

--- app/src/main/java/com/happyfi/publicactivex/activity/TaoBaoActivity.java
+++ app/src/main/java/com/happyfi/publicactivex/activity/TaoBaoActivity.java
@@ -139,7 +139,7 @@ public class TaoBaoActivity extends BaseActivity {
                         new Thread(new Runnable(){
                             public void run(){
                                 try {
-                                    Thread.sleep(1000);
+                                    Thread.sleep(2000);
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
